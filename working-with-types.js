3d45075eaf9b60cd1c9eddcd015ded5a53525b22
Gerald Montero <geraldmontero12@gmail.com>
2021-11-22 15:59:15 -0600

    exercises types

---
+++ working-with-types.js
@@ -0,0 +1,77 @@
+var a = 1;
+console.log( a );
+var b = a++;
+console.log( b );
+var c = ++a;
+console.log( c );
+var d = "hello";
+console.log( d );
+var e = false;
+console.log( e );
+d++;
+console.log( d );
+e++;
+console.log( e );
+var perplexed;
+console.log( perplexed );
+perplexed + 2;
+console.log( perplexed)
+var price = 2.7;
+console.log( price);
+price.toFixed(2);
+console.log( price);
+isNaN(0)
+console.log(isNaN(0))
+isNaN(1)
+console.log(isNaN(1))
+isNaN("")
+console.log(isNaN(""))
+isNaN("string")
+console.log("string")
+isNaN("0")
+console.log(isNaN("0"))
+isNaN("1")
+console.log(isNaN("1"))
+isNaN("3.145")
+console.log(isNaN("3.145"))
+isNaN(Number.MAX_VALUE)
+console.log(isNaN(Number.MAX_VALUE))
+isNaN(Infinity)
+console.log(isNaN())
+isNaN("true")
+console.log(isNaN())
+isNaN(true)
+console.log(isNaN(true))
+isNaN("false")
+console.log(isNaN("false"))
+isNaN(false)
+console.log( isNaN(false))
+// to illustrate why the isNaN() function is needed:
+NaN == NaN
+!true
+console.log(!true)
+!false
+console.log(!false)
+!!true
+console.log(!!true)
+!!false
+console.log(!!false)
+!!0
+console.log(!!0)
+!!-0
+console.log(!!-0)
+!!1
+console.log(!!1)
+!!-1
+console.log(!!-1)
+!!0.1
+console.log(!!0.1)
+!!"hello"
+
+!!""
+
+!!''
+
+!!"false"
+
+!!"0"
