fa972979f733a4226c6101f41bb82f7f3e1e3e8e
Gerald Montero <geraldmontero12@gmail.com>
2021-11-22 12:21:15 -0600

    intro

---
+++ strings.js
@@ -0,0 +1,12 @@
+var myName = " My name is Gerald Montero.\n ";
+
+var from = "I am originally from NJ but i live in Houston now.\n ";
+
+var Hobby = "My favorite hobby is cooking.\n ";
+
+var Aboutme = myName + from + Hobby;
+
+console.log(Aboutme );
+
+
+
