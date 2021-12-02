4b06a1811c4fa3dd7a742e265b17584d2f1b3df1
Gerald Montero <geraldmontero12@gmail.com>
2021-12-02 16:59:15 -0600

    update

---
+++ js/dec2exercises.js
@@ -0,0 +1,23 @@
+// // TODO: using prompt() and alert(), write a loop that will continue to ask for the correct password until the
+// // correct password is given.
+// let passWord = prompt("Enter correct password")
+// let correctPassword = ("password12");
+//
+//
+// while (passWord !== correctPassword);{
+//     alert("wrong ");
+//     passWord = prompt("try again")
+//
+// // }alert("password correct");
+// let dozens = 0;
+// for(let i = 0; i <= 48; i++){
+//     dozens = i/12;
+//     console.log("dozen: ", dozens);
+//     console.log("eggs: ", i);
+// }
+for (let counter =  1; counter<=5; counter++){
+    console.log('first counter: ' + counter);
+    for(let counterTwo = 5; counterTwo > 0; counterTwo--){
+        console.log('second counter: ' + counterTwo)
+    }
+}
---
+++ js/for_loops.js
@@ -0,0 +1,27 @@
+// // 2//
+// function showMultiplicationTable(number) {
+//     for(let i=1; i <= 10; i++){
+//                 console.log(parseInt(number) + " x " + i + ' = ' + (number * i))
+//
+//             }
+//         }
+// showMultiplicationTable(7)
+// // 3 //
+// let randomNum = Math.floor(Math.random() * (200 - 20) + 20)
+// const isEven = num => num % 2 === 0;
+// for(let x=1; x <= 10; x++){
+//     let num = randomNum * x;
+//   if(isEven(num) === true){
+//       console.log(num + ' is even')
+//   }
+//     console.log(num + ' is odd ')
+
+
+// 4 //
+for(let i=1; i <= 10; i++ ){
+    console.log(i);
+    for (let j= 1; j <= i ; j++){
+        console.log(j)
+    }
+
+}
