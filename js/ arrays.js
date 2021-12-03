0bf1bb27197a8f1052939997b5b6ec37c4e389f2
Gerald Montero <geraldmontero12@gmail.com>
2021-12-03 11:45:32 -0600

    update

---
+++ js/ arrays.js
@@ -0,0 +1,18 @@
+let students = ["Gerald", "mark", "Mina","Jason","Justin","justin"];
+console.log(students.length)
+
+let favFood = [ "empanadillas", "pizza","steak","crwafish", "pasta"];
+console.log(students[3])
+console.log(students)
+// concatenate the lastname to the first name
+// access Justin value
+//re-assign that value to equal justin + lastname
+students[4] = students[4] + " Sebastian"
+console.log(students[4])
+
+//give each favorite food a rating 1 -5
+favFood[0] = favFood[0] + " 2nd fave "
+favFood[1]= favFood[1] + " 3rd fave"
+console.log(favFood)
+let valIndexZero = students[0]
+console.log(valIndexZero)
