6b5e65410b55c52e4eb8956ac638f6e6620b6a9a
Gerald Montero <geraldmontero12@gmail.com>
2022-01-12 11:09:25 -0600

    morning notes

---
+++ js/dom-lecture.js
@@ -0,0 +1,26 @@
+//Change the span to say user input name
+
+
+// var promptAnswer = prompt("Enter Name Please");
+//
+// var retrievedSpan = document.getElementById('name');
+//
+// console.log("promptAnswer:", promptAnswer);
+// console.log("retrievedSpan:",retrievedSpan);
+//
+// retrievedSpan.textContent = promptAnswer;
+//
+// Document ----> the name if the DOM object
+// getElementById() ----> searches by Id WILL (AND SHOULD) ONLY RETURN ONE ELEMENT
+//getElementsByClass() ----> searches by Class ----> can return multiple
+//getElementByTageName() ----> searches by tag name
+
+// Change the text of the p tag
+// var newText = document.getElementById("changeText");
+//
+//     newText.innerText = " Today's Weather is chilly and cloudy";
+
+// loop through arrays to change each element returned
+// for (var i =0; i < myElemts.length; i++){
+//     myElemts[i].innerHTML = "hello world"
+// }
