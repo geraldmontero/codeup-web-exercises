c410755662e0ae26c9d31a68702da6193585c557
Gerald Montero <geraldmontero12@gmail.com>
2022-01-19 12:16:46 -0600

    j query

---
+++ js/jquerynote.js
@@ -0,0 +1,30 @@
+$("#p-one")
+console.log($("#p-one"))
+
+//
+// let heading = $("#heading")
+// console.log(heading)
+//
+// // add a class of border
+// $("#p-one").addClass("border");
+
+// add a border if logged in
+// check value of is logged in
+//grab value of isloggedin
+//save to a variable
+//compare value og logged in to true
+//add a border
+// assign a class to the element, if true
+
+
+let user1 = {
+    name: "Gerald",
+    isLoggedIn : true,
+    age: 31
+}
+
+let loginStatus = user1.isLoggedIn;
+
+if(loginStatus === true){
+    $("#heading").addClass("border")
+}
