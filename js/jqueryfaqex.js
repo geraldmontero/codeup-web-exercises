6d0ef1c3918a204b13d20429b751e80d88caedc1
Gerald Montero <geraldmontero12@gmail.com>
2022-01-25 11:08:39 -0600

    jquery exercises

--- js/jqueryfaqex.js
+++ js/jqueryfaqex.js
@@ -15,3 +15,17 @@ $("dd").addClass("invisible");
 $("dt").click(function () {
     $(this).toggleClass("highlight")
 });
+// button to make last li yellow
+$("#yellowli").click(function () {
+    $('li:last-child').toggleClass("highlight")
+});
+// bolded li after h3 clicked
+
+$('h3').click(function () {
+    $(this).next().children("li").toggleClass('bold')
+});
+//When any list item is clicked, first li of the parent ul should have a font color of blue.
+
+$('ul').click(function () {
+    $(this).children('li:first-child').toggleClass('blue')
+});
