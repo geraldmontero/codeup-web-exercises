c1e2ce42cb5a3b9cd05d3a59fe4f5c40242b447a
Gerald Montero <geraldmontero12@gmail.com>
2022-01-27 14:49:15 -0600

    Ajax Blog

---
+++ js/ajaxnote.js
@@ -0,0 +1,16 @@
+//AJAX
+
+// AJAX - asynchronous JavaScript and XML
+
+//xml - Extensible Markup Language -stores and transports data
+// js - logic language
+// Web Sever- server that holds your website hardware and
+// software (handles request, send data, it undersatnds URLS
+//HTTP Server -
+//JSON -
+
+// AJAX REQuest using jquery
+
+//Get information form random API
+$.ajax(url here)// syntax for ajx request
+$.ajax()
